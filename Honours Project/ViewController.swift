42f1d4105dcd14e904849af9401eff96f350ed66
Calum <Calum@iMac.local>
2015-03-23 20:41:36 +0000

    Parse now works, added dependencies, header file and simple test entry. (remember to keep spaces out of key names)

--- Honours Project/ViewController.swift
+++ Honours Project/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Parse.setApplicationId("nFHu3bnj37q5vzWkItvJutUFOPMLwjC1HbiiAXiC", clientKey: "TO9nrLJAi0w7X9pipEMjf1XgftWgfkAnuKkdhq6e")
+        
+        var test = PFObject(className: "Test")
+        test.setObject("iOS", forKey: "OperatingSystem")
+        test.saveInBackgroundWithBlock{
+            (success: Bool!, error: NSError!) -> Void in
+            
+            if(success==true){
+                println("Parse Object created: \(test.objectId)")
+            } else {
+                println(error)
+            }
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
