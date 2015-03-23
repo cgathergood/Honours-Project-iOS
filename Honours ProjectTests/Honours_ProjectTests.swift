172e721c2a2ee5abbaa105f9d77c117ae5f09a8e
Calum <Calum@iMac.local>
2015-03-23 20:13:03 +0000

    Initial Commit

---
+++ Honours ProjectTests/Honours_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Honours_ProjectTests.swift
+//  Honours ProjectTests
+//
+//  Created by Calum on 23/03/2015.
+//  Copyright (c) 2015 Gathergood. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Honours_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
