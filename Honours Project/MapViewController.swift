3b6359ee57e80e7e5c845c537ca68f62ae6392e4
Calum <calumgathergood@gmail.com>
2015-08-06 19:23:22 +0100

    Added Map functionality

---
+++ Honours Project/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  Honours Project
+//
+//  Created by Calum on 06/08/2015.
+//  Copyright (c) 2015 Gathergood. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+}
