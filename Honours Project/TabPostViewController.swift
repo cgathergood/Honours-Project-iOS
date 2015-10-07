e5312b551e9cb3280a1d87d2fc5048d39b794f2f
Calum <calumgathergood@gmail.com>
2015-10-07 23:11:52 +0100

    action sheet added with access to gallery and camera.

--- Honours Project/TabPostViewController.swift
+++ Honours Project/TabPostViewController.swift
@@ -24,6 +24,34 @@ class TabPostViewController: UIViewController {
     }
     @IBAction func getPhoto(sender: AnyObject) {
         
+        //Create the AlertController
+        let actionSheetController: UIAlertController = UIAlertController(title: "Choose Photo", message: "", preferredStyle: .ActionSheet)
+        
+        //Create and add the Cancel action
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+            //Just dismiss the action sheet
+        }
+        actionSheetController.addAction(cancelAction)
+        
+        let takePictureAction: UIAlertAction = UIAlertAction(title: "Take Photo", style: .Default) { action -> Void in
+            let image = UIImagePickerController()
+            image.sourceType = UIImagePickerControllerSourceType.Camera
+            
+            self.presentViewController(image, animated: true, completion: nil)
+        }
+        actionSheetController.addAction(takePictureAction)
+        
+        let choosePictureAction: UIAlertAction = UIAlertAction(title: "Choose Exisiting Photo", style: .Default) { action -> Void in
+            let image = UIImagePickerController()
+            image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            
+            self.presentViewController(image, animated: true, completion: nil)
+        }
+        actionSheetController.addAction(choosePictureAction)
+        
+        //Present the AlertController
+        self.presentViewController(actionSheetController, animated: true, completion: nil)
+        
     }
     
     @IBAction func post(sender: AnyObject) {
