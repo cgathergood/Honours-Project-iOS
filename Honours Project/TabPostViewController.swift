4d7b552eb0ff42c141501ce70d8195bd1df4b7f8
Calum <calumgathergood@gmail.com>
2015-10-07 22:54:07 +0100

    Created view controller for post. Set up outlets and functions for UI elements

---
+++ Honours Project/TabPostViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TabPostViewController.swift
+//  Honours Project
+//
+//  Created by Calum on 07/10/2015.
+//  Copyright © 2015 Gathergood. All rights reserved.
+//
+
+import UIKit
+
+class TabPostViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func getPhoto(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func post(sender: AnyObject) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
