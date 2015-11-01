38cc89f7d9d03fa3d6b04a452b46d14c9d490aa9
Calum <calumgathergood@gmail.com>
2015-11-01 22:05:29 +0000

    included ParseUI lib

---
+++ Honours Project/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  Honours Project
+//
+//  Created by Calum on 01/11/2015.
+//  Copyright © 2015 Gathergood. All rights reserved.
+//
+
+import UIKit
+
+class CustomClass: PFTableViewCell {
+    
+}
